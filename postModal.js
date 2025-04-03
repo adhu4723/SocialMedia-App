93643db4a5fc02d87a84baed1564cae06af201e7
adhu4723 <adarshdhanwis@gmail.com>
2025-04-03 09:27:06 +0530

    Create postModal.js

---
+++ postModal.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Users who liked the post
+    comments: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
